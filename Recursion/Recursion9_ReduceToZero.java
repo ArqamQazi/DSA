70a109eb4f9b4340f607bb8b81674272dab5d937
ArqamQazi <arqamqazi549@gmail.com>
2023-10-25 21:18:21 +0530

    Number of Steps to Reduce a Number to Zero

---
+++ Recursion/Recursion9_ReduceToZero.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class Recursion9_ReduceToZero {
+    public static void main(String[] args) {
+        int ans = numberOfSteps(20);
+        System.out.println(ans);
+    }
+    static int numberOfSteps(int num) {
+        return helper(num, 0);
+    }
+    static int helper(int num, int steps) {
+        if (num == 0) {
+            return steps;
+        }
+        if (num % 2 == 0) {
+            return helper(num/2, steps + 1);
+        }
+        return helper(num-1, steps + 1);
+    }
+}
